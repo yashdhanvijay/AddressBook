9b0908702f6cbbf0dbc908a6bc269a2d5d6a499c
yashdhanvijay <yash10dhanvijay.com>
2022-11-15 19:33:33 +0530

    [Yash] Add: Folder Structure created

---
+++ AddressBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Contact
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt32(Console.ReadLine()),
+                PhoneNumber = Console.ReadLine(),
+                Email = Console.ReadLine(),
+            };
+            Console.WriteLine("contact details" + "\n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip +"\n" + contact.PhoneNumber  +"\n" + contact.Email );
+        }
+    }
+}
