150e33b8cbcbc835a31c6507b54d6d63447333f8
yashdhanvijay <yash10dhanvijay.com>
2022-11-16 00:23:03 +0530

    [YASH]Add: Edit Contact Details

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -10,22 +10,26 @@ namespace AddressBook
             {
                 Console.WriteLine("Welcome to Address Book Problem Statement\n");
                 Console.WriteLine("Enter your Choice");
-                Console.WriteLine("1. Create a new Contact \n2. Exit");
+                Console.WriteLine("1. Create a new Contact \n2. Edit a Contact \n3. Exit \n");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine("Enter your details :-\n");
+                        Console.WriteLine("\nEnter your details :-\n");
                         Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
                                         "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
                                         "Phone Number - " + "\n" + "Email ID - ");
-                     
-                        
-                        break;
+                                   break;
+                    case 2:
+                        Console.WriteLine("\nEnter your details :-\n");
+                        Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
+                                        "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
+                                        "Phone Number - " + "\n" + "Email ID - ");
+                                   break;
                     default:
                         Console.WriteLine("Try Again");
                         condition = false;
-                        break;
+                                   break;
                 }
             }
         }
