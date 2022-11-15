b63793f85c79920a588d31291e933733aef99619
yashdhanvijay <yash10dhanvijay.com>
2022-11-16 00:29:34 +0530

    [YASH]Add: Delete Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -8,28 +8,29 @@ namespace AddressBook
             bool condition = true;
             while (condition)
             {
+               
                 Console.WriteLine("Welcome to Address Book Problem Statement\n");
                 Console.WriteLine("Enter your Choice");
-                Console.WriteLine("1. Create a new Contact \n2. Edit a Contact \n3. Exit \n");
+                Console.WriteLine("1. Create a new Contact \n2. Edit a Contact \n3. Delete a Contact \n4. Exit \n");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         Console.WriteLine("\nEnter your details :-\n");
-                        Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
-                                        "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
-                                        "Phone Number - " + "\n" + "Email ID - ");
-                                   break;
+                           break;
                     case 2:
+                        Console.WriteLine("\nEnter your details :-\n");                      
+                           break;
+                    case 3:
                         Console.WriteLine("\nEnter your details :-\n");
-                        Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
-                                        "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
-                                        "Phone Number - " + "\n" + "Email ID - ");
-                                   break;
+
+                        Console.WriteLine("\nEnter name whose detail you want to delete :-\n");
+                 
+                           break;
                     default:
                         Console.WriteLine("Try Again");
                         condition = false;
-                                   break;
+                        break;
                 }
             }
         }
